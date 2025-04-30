8f8bc6f309fcdb8d345758f30b1ba3a5f73f8f66
inchulkim95 <ink007@ucsd.edu>
2025-04-29 23:57:14 -0700

    try3wow

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -29,8 +29,7 @@ function discountPrices(prices, discount) {
     const length = prices.length;
 
     for(let i = 0; i < length; i++) {
-        let discountedPrice = 0;
-        discountedPrice += prices[i] * (1 - discount);
+        const discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
